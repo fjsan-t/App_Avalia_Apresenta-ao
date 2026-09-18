e7eea059da70fca820c17ace2a5095db8b150f2b
Flávia Santos <anafjs@ufba.br>
2026-09-18 11:25:39 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,57 +2,57 @@ const trabalhos = [
         {
             id: 1,
             titulo: "Inteligência Artificial Aplicada à Perícia de Comparação Facial ",
-            autor: "Antônio César Morant Braid (PC)",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Antônio César Morant Braid (PC)",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 2,
             titulo: "Restauração de Serial Obliterado em Chassi de Veículos Automotores.",
-            autor: "Sérgio Selma dos Santos (PC)",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Sérgio Selma dos Santos (PC)",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 3,
             titulo: "Reset de Fábrica em Dispositivos Móveis: Implicações Forenses na Destruição de Provas Digitais",
-            autor: "Marcelo A. Sampaio L. Costa (PC).",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Marcelo A. Sampaio L. Costa (PC).",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 4,
             titulo: "Perícia de Documentos Digitais: Proposta de Procedimento para Análise",
-            autor: "Lince Lima Santos (PC), Fabio Leandro dos Santos Correia (PML), Dayana Souza Barbosa Santos (PC)",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Lince Lima Santos (PC), Fabio Leandro dos Santos Correia (PML), Dayana Souza Barbosa Santos (PC)",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 5,
             titulo: "Palinologia Forense Aplicada na Determinação de Autoria de Furto por Meio de Simulação em Área Rural da Caatinga.",
-            autor: "Ernani Machado de Freitas Lins Neto (PC), Francisco Hilder Magalhães e Silva",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Ernani Machado de Freitas Lins Neto (PC), Francisco Hilder Magalhães e Silva",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 6,
             titulo: "Estimativa de Velocidades de Motocicletas em Atropelamentos com Vítimas Fatais: Dois Estudos de Caso",
-            autor: "Ernani Machado de Freitas Lins Neto (PC), Otaviano Bruno Ferreira Novais (PC), Ronielly Santana Coelho (PC), Ana Beatriz Rodrigues Massaranduba (PC), Mateus Souza Galvão (PC).",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Ernani Machado de Freitas Lins Neto (PC), Otaviano Bruno Ferreira Novais (PC), Ronielly Santana Coelho (PC), Ana Beatriz Rodrigues Massaranduba (PC), Mateus Souza Galvão (PC).",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 7,
             titulo: "Produção de Câmaras de Vaporização de Cianoacrilato de Baixo Custo: Impactos na Elucidação de Homicídios no Interior do Estado.",
-            autor: "Gerlon Santos Souza (PT).",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Gerlon Santos Souza (PT).",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         },
         {
             id: 8,
             titulo: "Aplicação Integrada da Osteologia e Toxicologia Forense na Investigação de Restos Mortais Animais: Relato de Caso.",
-            autor: "Lince Lima Santos (PC), Fabio Leandro dos Santos Correia (PML), Dayana Souza Barbosa Santos (PC).",
-            coautores: "Fulano, Sicrano",
+            apresentador: "Lince Lima Santos (PC), Fabio Leandro dos Santos Correia (PML), Dayana Souza Barbosa Santos (PC).",
+            autores: "Fulano, Sicrano",
             resumo: "Esse trabalho é sobre detalhe, detalhe."
         }
         
